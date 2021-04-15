1224e4e5fe306ba5080840acb871de607d015a54
sasha-kontsevich <sasha.kontsevich@gmail.com>
2021-04-15 08:37:52 +0300

    d

---
+++ frontend/views/site/friends.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Друзья';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-friends">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>На этой странице абсолютно ничего нет</p>
+
+    <code><?= __FILE__ ?></code>
+</div>
